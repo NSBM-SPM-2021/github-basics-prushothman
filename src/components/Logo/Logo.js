044d489ea65df704bcfc30aad24fe43c26e99d3b
prushothman <69847432+prushothman@users.noreply.github.com>
2021-09-02 16:00:37 +0530

    first

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classes from "./Logo.css";
-import burgerLogo from "../../assets/images/bur.jpg";
+import burgerLogo from "../../assets/images/burg.jpg";
 const Logo = () => {
   return (
     <div className={classes.Logo}>
